dd6e2be9e89dbf33bdcde435ca29f53548bc91b9
Gayoung Park <gayoungp@uw.edu>
2021-01-03 20:00:39 -0800

    added citation and SPD figure/ title inside

--- analysis/paper/008-summed-probability-distributions.R
+++ analysis/paper/008-summed-probability-distributions.R
@@ -170,16 +170,14 @@ aic_log <- -363176.8
 df_lin <- 3
 aic_lin <- -368425.3
 
+#make a string with the model stats for each plot title : basic figure
 par(mar=c(2,2,2,2))
 par(mfrow=c(4,1))
 
-#make a string with the model stats for each plot title
 plot(exp_test, main = paste0("Exponential Model (df = ", df_exp, ", AIC = ", aic_exp,")"))
 plot(uni_test, main = "Uniform Model")
 plot(lin_test, main = paste0("Linear Model (df = ", df_lin, ", AIC = ", aic_lin,")"))
 plot(log_test, main = paste0("Logistic Model (df = ", df_log, ", AIC = ", aic_log,")"))
-
-#dev.off()
 
 png(here::here("analysis/figures/008-summed-probability-distribution-models.png"),
     h = 5,
@@ -187,10 +185,70 @@ png(here::here("analysis/figures/008-summed-probability-distribution-models.png"
     units = "px")
 #ggsave(here::here("analysis/figures/008-summed-probability-distribution-models.png"))
 
+dev.off()
+
+#
 
 
-#save a table of AIC result as csv
-# write.csv(df_aic, file="analysis/figures/008-SPD-AIC.csv", row.names = FALSE)
+# Codes from https://zenodo.org/record/4322979#.X-4bH-lKgWo
+# fig 2
+# layout(matrix(c(1,2,3,4,5,6,7), 7, 1, byrow=TRUE), widths=6, heights=c(1,1,1,1,1,1,1.8))
+# par(mar=c(0, 1, 0, 1))
+# par(yaxs="i")
+# par(xaxs="i")
+#
+ymax <- max(ko.spd.smoothed$grid$PrDens)
 
-# knitr::kable(df_aic)
+plot(exp_test, ylim=c(0,ymax), xlim=c(50000,10000), drawaxes=FALSE )
+legend(x=49000, ymax*0.94, legend=c("SPD", "95% MC envelope","positive deviation","negative deviation"),col=c( "black","lightgrey",rgb(0.7,0,0,0.2),rgb(0,0,0.7,0.2)), lwd=c(0.5,5,5,5), cex=0.6, bg="white", title="")
+text(x=48000, ymax*0.90, labels="Exponential Model", font=2, cex=0.6, adj=c(0,0.7))
+text(x=48000, ymax*0.54, cex=0.5, font=2, adj=c(0,0),labels=paste0("(df = ", df_exp, ", AIC = ", aic_exp,")"))
+box()
+xticks <- seq(50000,10000,-10000)
+axis(side=1, at=xticks, labels=xticks, cex.axis=0.8)
+mtext("Years cal BP",1, 2.3, at=34000, adj=0, font=2, cex=0.8)
 
+dev.off()
+
+
+
+plot(exp_test, main = paste0("Exponential Model (df = ", df_exp, ", AIC = ", aic_exp,")"))
+plot(uni_test, main = "Uniform Model")
+plot(lin_test, main = paste0("Linear Model (df = ", df_lin, ", AIC = ", aic_lin,")"))
+plot(log_test, main = paste0("Logistic Model (df = ", df_log, ", AIC = ", aic_log,")"))
+
+
+# lines(expnull$fit$calBP,expnull$fit$PrDens, col="black", lty="dashed", lwd=0.5)
+# abline(v=seq(14000,2500,-500), lty="dotted", col="white")
+# legend(x=13800, ymax*0.94, legend=c("SPD (dates not normalised)","Exponential Model", "95% MC envelope","positive deviation","negative deviation"),col=c(1, "black","lightgrey",rgb(0.7,0,0,0.2),rgb(0,0,0.7,0.2)), lty=c(1,2,1,1,1), lwd=c(0.5,0.5,5,5,5), cex=0.6, bg="white", title="")
+# text(x=13700, ymax*0.92, labels="d. Exponential Fit", font=2, cex=0.8, adj=c(0,0.7))
+# text(x=13400, ymax*0.52, cex=0.7, adj=c(0,0.7), labels=substitute(paste(italic(p), "=", x, sep=""), list(x=round(expnull$pval,4))))
+# box()
+# xticks <- seq(14000,2450,-500)
+# axis(side=1, at=xticks, labels=xticks, las=2, cex.axis=0.8)# add BP axis
+#
+# axis(side=1, at=xticks-50, labels=xticks-2000, las=2, cex.axis=0.8,pos=-0.35)# add BC/AD axis
+# mtext("BC",1, 5.3, at=13725, adj=0, font=2, cex=0.5, las=2)
+# dev.off()
+#
+
+# plot(perm, focalm="1", xlim=c(14000,2400),col.obs="brown", lwd.obs=1, drawaxes=FALSE)
+# abline(v=seq(13500,3000,-500), lty="dotted", col="white")
+# legend(x=13800, y=ymax*0.90,legend=c("SPD","95% MC envelope","positive deviation","negative deviation"),col=c(1,"lightgrey",rgb(0.7,0,0,0.2),rgb(0,0,0.7,0.2)),lty=c(1,1,1,1),lwd=c(0.5,5,5,5),cex=0.6, bg="white", title=expression(bold("a. Anatolia")))
+# text(x=13800, y=ymax*0.42, labels=paste("n=",nrow(datesp[datesp$Region==1,]),", sites=",length(unique(datesp$SiteName[datesp$Region==1])),", bins=",length(unique(bins[datesp$Region==1])),sep=""), font=1, cex=0.6, adj=c(0,0.7))
+# text(x=13800, y=ymax*0.35, cex=0.6, adj=c(0,0.7), labels=substitute(paste(italic(p), "=", x, sep=""), list(x=round(min(perm$pValueList["1"],1-perm$pValueList["1"]),3))))
+# text(x=12500, y=ymax*0.14, labels="Epipalaeolithic",lty=c("solid"), col=c("brown"), font=2, cex=0.8, adj=c(0.5,-0.5))
+# segments(10300, ymax*0.15,10300,ymax*0.02, lty="solid", col="brown", lwd=(1.2))
+# text(x=9000, ymax*0.01, labels="Neolithic",lty=c("solid"), col=c("brown"), font=2, cex=0.8, adj=c(0.5,-0.5))
+# segments(8000, ymax*0.15,8000,ymax*0.02, lty="solid", col="brown", lwd=(1.2))
+# text(x=6500, ymax*0.01, labels="Chalcolithic",lty=c("solid"), col=c("brown"), font=2, cex=0.8, adj=c(0.5,-0.5))
+# segments(5000, ymax*0.15,5000,ymax*0.01, lty="solid", col="brown", lwd=(1.2))
+# segments(4000, ymax*0.12,4000,ymax*0.01, lty="dotted", col="brown", lwd=(1.2))
+# segments(3400, ymax*0.12,3400,ymax*0.01, lty="dotted", col="brown", lwd=(1.2))
+# segments(3100, ymax*0.15,3100,ymax*0.02, lty="solid", col="brown", lwd=(1.2))
+# text(x=4000, ymax*0.13, labels="Bronze Age",lty=c("solid"), col=c("brown"), font=2, cex=0.9, adj=c(0.5,-0.5))
+# text(x=4500, ymax*0.01, labels="EBA",lty=c("solid"), col=c("brown"), font=2, cex=0.6, adj=c(0.5,-0.5))
+# text(x=3750, ymax*0.01, labels="MBA",lty=c("solid"), col=c("brown"), font=2, cex=0.5, adj=c(0.5,-0.5))
+# text(x=3250, ymax*0.01, labels="LBA",lty=c("solid"), col=c("brown"), font=2, cex=0.5, adj=c(0.5,-0.5))
+# text(x=2850, 0, labels="Iron\nAge",lty=c("solid"), col=c("brown"), font=2, cex=0.8, adj=c(0.2,-0.2))
+# box()
