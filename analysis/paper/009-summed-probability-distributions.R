f90abd1937dcb81c95e7e9d8a5bb8249a4f0edf6
Gayoung Park <gayoungp@uw.edu>
2021-04-12 15:13:51 -0700

    Wrote acknowledgements. Added missed codes (moved to the new spd script) to the main spd script to run all the codes

--- analysis/paper/009-summed-probability-distributions.R
+++ analysis/paper/009-summed-probability-distributions.R
@@ -3,6 +3,17 @@ library(glue)
 library(ggrepel)
 library(here)
 
+dates <-
+  read.csv(here("analysis/data/raw_data/Radiocarbon dates.csv"))
+
+dates_clean <-
+  dates %>%
+  mutate(age = parse_number(as.character(age)),
+         error = parse_number(as.character(error))) %>%
+  # filter age range here
+  filter(age < 50500,
+         age >= 10000,
+         method == 'AMS')
 
 # devtools::install_github('ahb108/rcarbon')
 library(rcarbon)
