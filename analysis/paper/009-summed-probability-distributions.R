94215c4e3fdc3960e8488dbe4d9eea9d6ff45e78
Ben Marwick <benmarwick@gmail.com>
2021-09-27 09:15:49 -0700

    simplify 008 and 009

--- analysis/paper/009-summed-probability-distributions.R
+++ analysis/paper/009-summed-probability-distributions.R
@@ -4,30 +4,8 @@ library(ggrepel)
 library(here)
 library(cowplot)
 
-
-# import the data
-dates <-
-  read.csv(here("analysis/data/raw_data/Radiocarbon dates_revision.csv"))
-
-dates_clean <-
-  dates %>%
-  mutate(age = parse_number(as.character(age)),
-         error = parse_number(as.character(error))) %>%
-  # filter age range here
-  filter(age < 50500,
-         age >= 10000,
-         method == 'AMS') %>%   #exclude OSL dates
-  filter(!site_name_original %in% c('Nobong',
-                                    'Geoduri',
-                                    'Geumneungdong',
-                                    'Janggi',
-                                    'Sanggari',
-                                    'Sinheug',
-                                    'Oeryang',
-                                    'Eosanri',
-                                    'Jeommal',
-                                    'Pyeongneungdong'))#exclude sites that have only one date
-
+# read in spreadsheet and clean dates data
+source(here::here("analysis/paper/008-summed-probability-distributions.R"))
 
 # devtools::install_github('ahb108/rcarbon')
 library(rcarbon)
